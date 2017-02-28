3182ef69c37472d36ef20672c72dccba73a84e9e
hug-dsi <gmer@hcuge.ch>
2017-02-28 17:45:20 +0100

    correction lien vers la licence

--- src/common/core/graphics/color.ts
+++ src/common/core/graphics/color.ts
@@ -4,7 +4,7 @@
  *  Copyright Hôpitaux Universitaires de Genève All Rights Reserved.
  *
  *  Use of this source code is governed by an Apache-2.0 license that can be
- *  found in the LICENSE file at https://github.com/DSI-HUG/deja-js/blob/master/LICENSE
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  * /
  *
  */
