8d3c19b33eb06b9634ec22146a6efcaec6eaedb4
Serge <sdil@hcuge.ch>
2017-03-13 13:07:20 +0100

    Compile issue after NPM install/update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,7 +33,9 @@ import 'core-js/es6/set';
 import 'core-js/es6/string';
 import 'core-js/es6/symbol';
 
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+
 import 'zone.js/dist/zone';
 
 import 'custom-event-polyfill/custom-event-polyfill';
