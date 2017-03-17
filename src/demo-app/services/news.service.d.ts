0dbf84e5accb7591096bbbb00e2909e3473e7f0e
Travis CI <travis@hcuge.ch>
2017-03-17 15:15:06 +0000

    Deploy to GitHub Pages: 9ffda91167f865d6e88fbbad9b422852fa7f0216

---
+++ src/demo-app/services/news.service.d.ts
@@ -0,0 +1,26 @@
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+export declare class NewsService {
+    private http;
+    constructor(http: Http);
+    getNews$(): Observable<INews[]>;
+}
+export interface INewsResponse {
+    status: string;
+    sources: INews[];
+}
+export interface INews {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+    urlsToLogos: {
+        small: string;
+        medium: string;
+        large: string;
+    };
+    sortBysAvailable: string[];
+}
