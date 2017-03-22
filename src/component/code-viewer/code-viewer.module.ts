e4929fb456f1cb9c4541e9459bca20711364f8fd
rtrompier <remy.trompier@gmail.com>
2017-03-22 13:39:28 +0100

    Fix import JS Lib

--- src/component/code-viewer/code-viewer.module.ts
+++ src/component/code-viewer/code-viewer.module.ts
@@ -1,5 +1,15 @@
-import { NgModule } from '@angular/core';
+/*
+ * *
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ * /
+ *
+ */
 
+import { NgModule } from '@angular/core';
 import { DejaCodeViewerComponent } from './code-viewer.component';
 
 @NgModule({
