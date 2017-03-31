2496b52c55d47547dbf89ffdd8269141d9821b7d
Maxime F <flahou@hotmail.fr>
2017-03-31 15:16:33 +0200

    Import theme with webpack

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,10 +5,7 @@ var path = require("path");
 module.exports = {
     entry: {
         bundle: [
-            // "./node_modules/@angular/material/core/theming/prebuilt/deeppurple-amber.css",
-            // "./node_modules/@angular/material/core/theming/all-theme",
             "./src/scss",
-            // "./src/scss/variables/themeHUG.scss",
             "./src/polyfills.ts",
             "./src/demo-app/main.ts",
             "./send-action.js",
@@ -34,9 +31,7 @@ module.exports = {
             test: /\.(png|jpe?g|gif|ico)$/,
         }, {
             include: [
-                // path.resolve("./node_modules/@angular/material/core/theming/prebuilt"),
                 path.resolve("./src/scss"),
-                // path.resolve("./src/scss/variables"),
             ],
             loaders: ["style-loader", "css-loader", "resolve-url-loader"],
             test: /\.css$/,
