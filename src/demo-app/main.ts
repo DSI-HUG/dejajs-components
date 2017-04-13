1195c656ca55b22856df1f214573e3d439f258a7
rtrompier <remy.trompier@gmail.com>
2017-04-13 17:06:46 +0200

    Hot reload + e2e config

--- src/demo-app/main.ts
+++ src/demo-app/main.ts
@@ -13,5 +13,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DemoAppModule } from './demo-app-module';
 
+// Enables Hot Module Replacement.
+declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+}
+
 // enableProdMode();
 platformBrowserDynamic().bootstrapModule(DemoAppModule);
