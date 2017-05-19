0172c7a3f24745801852140cf773e2a56f1160c7
rtrompier <remy.trompier@gmail.com>
2017-05-19 21:40:17 +0200

    fix(Global): Better imports for compiler-cli compliance

--- src/component/data-grid/data-grid-row/data-grid-rows-event.ts
+++ src/component/data-grid/data-grid-row/data-grid-rows-event.ts
@@ -6,7 +6,7 @@
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
 
-import { DejaItemsEvent } from '../../../common/core/item-list';
+import { DejaItemsEvent } from '../../../common/core/item-list/index';
 
 export class DejaGridRowsEvent extends DejaItemsEvent {
 
