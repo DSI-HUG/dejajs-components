67fa043cba46e7e61ec5e33c89f03228103c88a2
Maxime F <flahou@hotmail.fr>
2017-05-22 11:41:27 +0200

    refactor(Global): Better imports

--- src/component/autosize-textarea/index.ts
+++ src/component/autosize-textarea/index.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdInputModule } from '@angular/material';
-import { DejaAutosizeTextAreaDirective } from './index';
+import { DejaAutosizeTextAreaDirective } from './autosize-textarea.directive';
 
 @NgModule({
     declarations: [DejaAutosizeTextAreaDirective],
