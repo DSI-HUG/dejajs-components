8cec8932347e4c8ff5411d7f76040b52cac10e91
rtrompier <remy.trompier@gmail.com>
2017-05-26 14:03:34 +0200

    ci(Bundle): Improve bundle size

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 /* eslint-disable */
 var gulp = require('gulp'),
-  path = require('path'),
-  ngc = require('@angular/compiler-cli/src/main').main,
-  rollup = require('gulp-rollup'),
-  rename = require('gulp-rename'),
-  del = require('del'),
-  runSequence = require('run-sequence'),
-  inlineResources = require('./tools/gulp/inline-resources'),
-  fs = require('fs'),
-  jeditor = require("gulp-json-editor");
+    path = require('path'),
+    ngc = require('@angular/compiler-cli/src/main').main,
+    rollup = require('gulp-rollup'),
+    uglify =  require('rollup-plugin-uglify'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    inlineResources = require('./tools/gulp/inline-resources'),
+    fs = require('fs'),
+    jeditor = require("gulp-json-editor");
 
 const rootFolder = path.join(__dirname);
 const srcFolder = path.join(rootFolder, 'src');
@@ -20,7 +21,7 @@ const distFolder = path.join(rootFolder, 'dist');
  * 1. Delete /dist folder
  */
 gulp.task('clean:dist', function () {
-  return deleteFolders([distFolder]);
+    return deleteFolders([distFolder]);
 });
 
 /**
@@ -29,8 +30,8 @@ gulp.task('clean:dist', function () {
  *    when copying to /.tmp.
  */
 gulp.task('copy:source', function () {
-  return gulp.src([`${srcFolder}/**/*`, `!${srcFolder}/node_modules`])
-    .pipe(gulp.dest(tmpFolder));
+    return gulp.src([`${srcFolder}/**/*`, `!${srcFolder}/node_modules`])
+        .pipe(gulp.dest(tmpFolder));
 });
 
 /**
@@ -38,8 +39,8 @@ gulp.task('copy:source', function () {
  *    We do this on the /.tmp folder to avoid editing the original /src files
  */
 gulp.task('inline-resources', function () {
-  return Promise.resolve()
-    .then(() => inlineResources(tmpFolder));
+    return Promise.resolve()
+        .then(() => inlineResources(tmpFolder));
 });
 
 
@@ -48,16 +49,16 @@ gulp.task('inline-resources', function () {
  *    compiled modules to the /build folder.
  */
 gulp.task('ngc', function () {
-  return ngc({
-    project: `${tmpFolder}/tsconfig.es5.json`
-  })
-    .then((exitCode) => {
-      if (exitCode === 1) {
-        // This error is caught in the 'compile' task by the runSequence method callback
-        // so that when ngc fails to compile, the whole compile process stops running
-        throw new Error('ngc compilation failed');
-      }
-    });
+    return ngc({
+        project: `${tmpFolder}/tsconfig.es5.json`
+    })
+        .then((exitCode) => {
+            if (exitCode === 1) {
+                // This error is caught in the 'compile' task by the runSequence method callback
+                // so that when ngc fails to compile, the whole compile process stops running
+                throw new Error('ngc compilation failed');
+            }
+        });
 });
 
 /**
@@ -65,39 +66,39 @@ gulp.task('ngc', function () {
  *    generated file into the /dist folder
  */
 gulp.task('rollup:fesm', function () {
-  return gulp.src(`${buildFolder}/**/*.js`)
-  // transform the files here.
-    .pipe(rollup({
+    return gulp.src(`${buildFolder}/**/*.js`)
+        // transform the files here.
+        .pipe(rollup({
 
-      // Bundle's entry point
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
-      entry: `${buildFolder}/index.js`,
+            // Bundle's entry point
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
+            entry: `${buildFolder}/index.js`,
 
-      // A list of IDs of modules that should remain external to the bundle
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
-      external: [
-        '@angular/core',
-        '@angular/common',
-        '@angular/forms',
-        '@angular/material',
-        '@angular/platform-browse',
-        'rxjs/Observable',
-        'rxjs/Subject',
-        'rxjs/BehaviorSubject',
-        'rxjs/add/operator/toPromise',
-        'moment',
-        'angular2-text-mask'
-      ],
+            // A list of IDs of modules that should remain external to the bundle
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
+            external: [
+                '@angular/core',
+                '@angular/common',
+                '@angular/forms',
+                '@angular/material',
+                '@angular/platform-browse',
+                'rxjs/Observable',
+                'rxjs/Subject',
+                'rxjs/BehaviorSubject',
+                'rxjs/add/operator/toPromise',
+                'moment',
+                'angular2-text-mask'
+            ],
 
-      // commonjs: {
-      //   include: 'node_modules/**'
-      // },
+            // commonjs: {
+            //   include: 'node_modules/**'
+            // },
 
-      // Format of generated bundle
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
-      format: 'es'
-    }))
-    .pipe(gulp.dest(distFolder));
+            // Format of generated bundle
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
+            format: 'es'
+        }))
+        .pipe(gulp.dest(distFolder));
 });
 
 /**
@@ -105,51 +106,55 @@ gulp.task('rollup:fesm', function () {
  *    generated file into the /dist folder
  */
 gulp.task('rollup:umd', function () {
-  return gulp.src(`${buildFolder}/**/*.js`)
-  // transform the files here.
-    .pipe(rollup({
+    return gulp.src(`${buildFolder}/**/*.js`)
+        // transform the files here.
+        .pipe(rollup({
 
-      // Bundle's entry point
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
-      entry: `${buildFolder}/index.js`,
+            // Bundle's entry point
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#entry
+            entry: `${buildFolder}/index.js`,
 
-      // A list of IDs of modules that should remain external to the bundle
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
-      external: [
-        '@angular/core',
-        '@angular/common',
-        '@angular/forms',
-        '@angular/material',
-        '@angular/platform-browse',
-        'rxjs/Observable',
-        'rxjs/Subject',
-        'rxjs/BehaviorSubject',
-        'rxjs/add/operator/toPromise',
-        'moment',
-        'angular2-text-mask'
-      ],
+            // A list of IDs of modules that should remain external to the bundle
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#external
+            external: [
+                '@angular/core',
+                '@angular/common',
+                '@angular/forms',
+                '@angular/material',
+                '@angular/platform-browse',
+                'rxjs/Observable',
+                'rxjs/Subject',
+                'rxjs/BehaviorSubject',
+                'rxjs/add/operator/toPromise',
+                'moment',
+                'angular2-text-mask'
+            ],
 
-      // Format of generated bundle
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
-      format: 'umd',
+            // Format of generated bundle
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#format
+            format: 'umd',
 
-      // Export mode to use
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#exports
-      exports: 'named',
+            // Export mode to use
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#exports
+            exports: 'named',
 
-      // The name to use for the module for UMD/IIFE bundles
-      // (required for bundles with exports)
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
-      moduleName: 'dejajs-component',
+            // The name to use for the module for UMD/IIFE bundles
+            // (required for bundles with exports)
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
+            moduleName: 'dejajs-component',
 
-      // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
-      globals: {
-        typescript: 'ts'
-      }
+            // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
+            globals: {
+                typescript: 'ts'
+            },
 
-    }))
-    .pipe(rename('dejajs-component.umd.js'))
-    .pipe(gulp.dest(distFolder));
+            plugins: [
+                uglify(),
+            ]
+
+        }))
+        .pipe(rename('dejajs-component.umd.js'))
+        .pipe(gulp.dest(distFolder));
 });
 
 /**
@@ -158,8 +163,8 @@ gulp.task('rollup:umd', function () {
  *    on step 5.
  */
 gulp.task('copy:build', function () {
-  return gulp.src([`${buildFolder}/**/*`, `!${buildFolder}/**/*.js`])
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([`${buildFolder}/**/*`, `!${buildFolder}/**/*.js`])
+        .pipe(gulp.dest(distFolder));
 });
 
 
@@ -167,8 +172,8 @@ gulp.task('copy:build', function () {
  * Copy all theming sccs files into the lib
  */
 gulp.task('copy:theming', function () {
-  return gulp.src([`${srcFolder}/**/*-theme.scss`])
-    .pipe(gulp.dest(`${distFolder}`));
+    return gulp.src([`${srcFolder}/**/*-theme.scss`])
+        .pipe(gulp.dest(`${distFolder}`));
 });
 
 
@@ -176,8 +181,8 @@ gulp.task('copy:theming', function () {
  * Copy the scss directory into the dist folder
  */
 gulp.task('copy:scss', function () {
-  return gulp.src([`${srcFolder}/scss/**/*`])
-    .pipe(gulp.dest(`${distFolder}/scss`));
+    return gulp.src([`${srcFolder}/scss/**/*`])
+        .pipe(gulp.dest(`${distFolder}/scss`));
 });
 
 
@@ -185,79 +190,79 @@ gulp.task('copy:scss', function () {
  * 8. Copy package.json from /src to /dist
  */
 gulp.task('copy:manifest', function () {
-  return gulp.src([`${srcFolder}/package.json`])
-  .pipe(jeditor(function(json) {
-      var package = JSON.parse(fs.readFileSync(path.join(rootFolder, 'package.json')));
-      json.version = package.version;
-      json.dependencies = package.dependencies;
-      return json;
-    }))
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([`${srcFolder}/package.json`])
+        .pipe(jeditor(function (json) {
+            var package = JSON.parse(fs.readFileSync(path.join(rootFolder, 'package.json')));
+            json.version = package.version;
+            json.dependencies = package.dependencies;
+            return json;
+        }))
+        .pipe(gulp.dest(distFolder));
 });
 
 /**
  * Copy sendAction to /dist
  */
 gulp.task('copy:assets', function () {
-  return gulp.src([
-    path.join(rootFolder, 'send-action.js'),
-    path.join(rootFolder, 'polyfills.ts'),
-  ]).pipe(gulp.dest(distFolder));
+    return gulp.src([
+        path.join(rootFolder, 'send-action.js'),
+        path.join(rootFolder, 'polyfills.ts'),
+    ]).pipe(gulp.dest(distFolder));
 });
 
 /**
  * 9. Copy README.md from / to /dist
  */
 gulp.task('copy:readme', function () {
-  return gulp.src([path.join(rootFolder, 'README.MD')])
-    .pipe(gulp.dest(distFolder));
+    return gulp.src([path.join(rootFolder, 'README.MD')])
+        .pipe(gulp.dest(distFolder));
 });
 
 /**
  * 10. Delete /.tmp folder
  */
 gulp.task('clean:tmp', function () {
-  return deleteFolders([tmpFolder]);
+    return deleteFolders([tmpFolder]);
 });
 
 /**
  * 11. Delete /build folder
  */
 gulp.task('clean:build', function () {
-  return deleteFolders([buildFolder]);
+    return deleteFolders([buildFolder]);
 });
 
 gulp.task('compile', function () {
-  runSequence(
-    'clean:dist',
-    'copy:source',
-    'inline-resources',
-    'ngc',
-    'rollup:fesm',
-    'rollup:umd',
-    'copy:scss',
-    'copy:theming',
-    'copy:build',
-    'copy:manifest',
-    'copy:readme',
-    'copy:assets',
-    'clean:build',
-    'clean:tmp',
-    function (err) {
-      if (err) {
-        console.log('ERROR:', err.message);
-        deleteFolders([distFolder, tmpFolder, buildFolder]);
-      } else {
-        console.log('Compilation finished succesfully');
-      }
-    });
+    runSequence(
+        'clean:dist',
+        'copy:source',
+        'inline-resources',
+        'ngc',
+        'rollup:fesm',
+        'rollup:umd',
+        'copy:scss',
+        'copy:theming',
+        'copy:build',
+        'copy:manifest',
+        'copy:readme',
+        'copy:assets',
+        'clean:build',
+        'clean:tmp',
+        function (err) {
+            if (err) {
+                console.log('ERROR:', err.message);
+                deleteFolders([distFolder, tmpFolder, buildFolder]);
+            } else {
+                console.log('Compilation finished succesfully');
+            }
+        });
 });
 
 /**
  * Watch for any change in the /src folder and compile files
  */
 gulp.task('watch', function () {
-  gulp.watch(`${srcFolder}/**/*`, ['compile']);
+    gulp.watch(`${srcFolder}/**/*`, ['compile']);
 });
 
 gulp.task('clean', ['clean:dist', 'clean:tmp', 'clean:build']);
@@ -270,5 +275,5 @@ gulp.task('default', ['build:watch']);
  * Deletes the specified folder
  */
 function deleteFolders(folders) {
-  return del(folders);
+    return del(folders);
 }
