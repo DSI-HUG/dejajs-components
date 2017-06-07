8cf166705804335784ad669ff38942fa01affca9
Maxime F <flahou@hotmail.fr>
2017-06-07 09:51:13 +0200

    docs(AccordionComponent): comment

--- src/component/accordion/accordion.component.ts
+++ src/component/accordion/accordion.component.ts
@@ -8,6 +8,9 @@
 
 import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
+/**
+ * Accordion component for Angular
+ */
 @Component({
     encapsulation: ViewEncapsulation.None,
     selector: 'deja-accordion',
@@ -15,26 +18,50 @@ import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '
     template: '<ng-content></ng-content>',
 })
 export class DejaAccordionComponent {
+    /**
+     * Internal use. Add accordion class to :host to avoid useless DOM
+     */
+    @HostBinding('class.accordion') protected accordionClass = true;
     private groups: DejaAccordionGroupComponent[] = [];
-    @HostBinding('class.accordion') true;
 
+    /**
+     * Add a group to the current Accordion Component
+     *
+     * @param {DejaAccordionGroupComponent} group
+     */
     public addGroup(group: DejaAccordionGroupComponent): void {
         this.groups.push(group);
     }
 }
 
+/**
+ * Accordion-group component for Angular
+ * Work inside an AccordionComponent
+ */
 @Component({
     selector: 'deja-accordion-group',
     template: '<ng-content></ng-content>',
 })
 export class DejaAccordionGroupComponent {
+    /**
+     * Status of the accordion group.
+     */
     @HostBinding('class.open') @Input() public isOpen: boolean;
-    @HostBinding('class.accordion-group') true;
+    /**
+     * Internal use. Add accordion-group class to :host to avoid useless DOM
+     */
+    @HostBinding('class.accordion-group') protected accordionGroupClass = true;
 
     constructor(private accordion: DejaAccordionComponent) {
         this.accordion.addGroup(this);
     }
 
+    /**
+     * Subscription to "click" event.
+     * Check if click is on header and then it toggle the group.
+     *
+     * @param {MouseEvent} event
+     */
     @HostListener('click', ['$event'])
     public toggleOpen(event: MouseEvent): void {
 
@@ -50,18 +77,32 @@ export class DejaAccordionGroupComponent {
     }
 }
 
+/**
+ * Accordion component for Angular
+ * Work inside AccordionGroupComponent
+ */
 @Component({
     selector: 'deja-accordion-header',
     template: `<ng-content></ng-content>`,
 })
 export class DejaAccordionHeaderComponent {
-    @HostBinding('class.accordion-header') true;
+    /**
+     * Internal use. Add accordion-header class to :host to avoid useless DOM
+     */
+    @HostBinding('class.accordion-header') protected accordionHeaderClass = true;
 }
 
+/**
+ * Accordion component for Angular
+ * Work inside AccordionGroupComponent (behind AccordionHeaderComponent)
+ */
 @Component({
     selector: 'deja-accordion-body',
     template: '<ng-content></ng-content>',
 })
 export class DejaAccordionBodyComponent {
-    @HostBinding('class.accordion-body') true;
+    /**
+     * Internal use. Add accordion-body class to :host to avoid useless DOM
+     */
+    @HostBinding('class.accordion-body') protected accordionBodyClass = true;
 }
