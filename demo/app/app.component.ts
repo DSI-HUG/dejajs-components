3cb545e2b10527bd721514d5b5f27f0db4fe72b7
Serge <sdil@hcuge.ch>
2017-07-19 17:31:27 +0200

    fix(DemoApp) Toolbar alignments and select size

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -12,15 +12,6 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
-class Color {
-    public label: string;
-    public value: string;
-    constructor(label?: string, value?: string) {
-        this.label = label;
-        this.value = value;
-    }
-}
-
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
@@ -32,40 +23,33 @@ export class AppComponent implements OnDestroy {
     public version: string;
     protected navOpened = true;
 
-    protected colors = [
-        new Color('HUG', 'hug'),
-        new Color('Pink', 'pink'),
-        new Color('Teal', 'teal'),
-        new Color('Amber', 'amber'),
-    ];
-
-    private _theme: Color;
-    protected theme$: BehaviorSubject<Color>;
+    private _theme: string;
+    protected theme$: BehaviorSubject<string>;
     private theme$sub: Subscription;
 
     constructor(elementRef: ElementRef) {
         const elem = elementRef.nativeElement as HTMLElement;
         try {
-            this._theme = this.colors.find((color) => color.value === localStorage.getItem('dejajs-demo-color'));
+            this._theme = localStorage.getItem('dejajs-demo-color');
         } catch (_e) {
             console.log('Fail to get your prefered color from the local storage.');
         }
 
         if (!this._theme) {
-            this._theme = this.colors[0];
+            this._theme = 'hug';
         }
         this.theme$ = new BehaviorSubject<any>(this._theme);
-        this.theme$sub = Observable.from(this.theme$).subscribe((theme) => elem.setAttribute('theme', theme.value));
+        this.theme$sub = Observable.from(this.theme$).subscribe((theme) => elem.setAttribute('theme', theme));
     }
 
     public get theme() {
         return this._theme;
     }
 
-    public set theme(theme: any) {
+    public set theme(theme: string) {
         this._theme = theme;
         try {
-            localStorage.setItem('dejajs-demo-color', theme.value);
+            localStorage.setItem('dejajs-demo-color', theme);
         } catch (_e) {
             console.log('Fail to set your prefered color to the local storage.');
         }
