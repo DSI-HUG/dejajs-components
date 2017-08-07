70d5dcdc2cce9435fe8d4afa07d99cda17aaa10c
rtrompier <remy.trompier@gmail.com>
2017-08-07 09:43:54 +0200

    quiet(License): Add license

--- src/component/tag/tag.component.ts
+++ src/component/tag/tag.component.ts
@@ -1,3 +1,10 @@
+/*
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
 import { Component, HostBinding, Input, Optional, Self } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { KeyCodes } from '../../common/core/keycodes.enum';
