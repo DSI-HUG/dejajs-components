f5110748d21488de16d3c7466ad9462bb9df6c7a
rtrompier <remy.trompier@gmail.com>
2017-09-26 16:24:24 +0200

    test(DejaTag): Improve test

--- src/component/tag/tag.component.spec.ts
+++ src/component/tag/tag.component.spec.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { MdIconModule, MdInputModule } from '@angular/material';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KeyCodes } from '../../common/core/keycodes.enum';
 import { DejaChipsModule } from '../chips/index';
 import { DejaTagComponent } from './tag.component';
 
@@ -81,6 +82,24 @@ describe('DejaTagComponent', () => {
         expect(getNbItems()).toEqual(0);
 
         comp.onAddItem('');
+        fixture.detectChanges();
+        expect(getNbItems()).toEqual(0);
+    });
+
+    it('should add item on keypress Enter', () => {
+        expect(getNbItems()).toEqual(0);
+        const tmp = { keyCode: KeyCodes.Enter, target: { value: 'HTML5' } } as any;
+
+        comp.onKeyDown(tmp);
+        fixture.detectChanges();
+        expect(getNbItems()).toEqual(1);
+    });
+
+    it('should not add item on keypress Ctrl', () => {
+        expect(getNbItems()).toEqual(0);
+        const tmp = { keyCode: KeyCodes.Ctrl, target: { value: 'HTML5' } } as any;
+
+        comp.onKeyDown(tmp);
         fixture.detectChanges();
         expect(getNbItems()).toEqual(0);
     });
