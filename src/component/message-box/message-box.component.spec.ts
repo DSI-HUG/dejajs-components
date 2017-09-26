4eb8dce9c667160b9020ecbecba776ea6a1e98bd
Cédric Déturche <cedric.deturche@gmail.com>
2017-09-26 22:49:44 +0200

    test(DejaMessageBox): Add unit test
    Add first asserts on the component
    
    test(DejaMessageBox): Override ngOnInit to set icon to null
    
    test(DejaMessageBox): Remove comments
    
    test(DejaMessageBox): Remove console.log
    
    docs(DejaMessageBox): Add license on spec

---
+++ src/component/message-box/message-box.component.spec.ts
@@ -0,0 +1,153 @@
+/*
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DejaMessageBoxComponent } from './message-box.component';
+
+describe('DejaMessageBoxComponent', () => {
+
+    let component: DejaMessageBoxComponent;
+    let fixture: ComponentFixture<DejaMessageBoxComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                DejaMessageBoxComponent
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DejaMessageBoxComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('input type', () => {
+        it('should set an info_outline icon if type is info', () => {
+            component.icon = null;
+            component.type = 'info';
+            fixture.detectChanges();
+            expect(component.icon).toEqual('info_outline');
+        });
+
+        it('should set an info_outline icon if type is primary', () => {
+            component.icon = null;
+            component.type = 'primary';
+            fixture.detectChanges();
+            expect(component.icon).toEqual('info_outline');
+        });
+
+        it('should set a check icon if type is success', () => {
+            component.icon = null;
+            component.type = 'success';
+            fixture.detectChanges();
+            expect(component.icon).toEqual('check');
+        });
+
+        it('should set a warning icon if type is warn', () => {
+            component.icon = null;
+            component.type = 'warn';
+            fixture.detectChanges();
+            expect(component.icon).toEqual('warning');
+        });
+
+        it('should set an error_outline icon if type is danger', () => {
+            component.icon = null;
+            component.type = 'danger';
+            fixture.detectChanges();
+            expect(component.icon).toEqual('error_outline');
+        });
+    });
+
+    describe('input title', () => {
+        it('should display title in md-card-title', () => {
+            component.type = 'warn';
+            component.title = 'Delete order';
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-title > span')).nativeElement;
+            expect(el.innerHTML).toEqual('Delete order');
+        });
+
+        it('should display title in md-card-content in horizontalMode', () => {
+            component.type = 'warn';
+            component.title = 'Delete order';
+            component.horizontal = true;
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-content > h2')).nativeElement;
+            expect(el.innerHTML).toEqual('Delete order');
+        });
+    });
+
+    describe('input icon', () => {
+        it('should display icon in md-card-title', () => {
+            component.icon = 'randomIcon';
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-title > md-icon')).nativeElement;
+            expect(el.innerHTML).toEqual('randomIcon');
+        });
+
+        it('should not display md-card-title if no icon', () => {
+            component.icon = null;
+            fixture.detectChanges();
+            expect(fixture.debugElement.query(By.css('md-card-title'))).toBeNull();
+        });
+    });
+
+    describe('input actions', () => {
+        it('should display a button for each action in md-card-actions', () => {
+            component.actions = [
+                {text: 'Yes', 'type': 'info', action: () => {}},
+                {text: 'No', type: 'warn', action: () => {}},
+            ];
+            fixture.detectChanges();
+
+            const els = fixture.debugElement.queryAll(By.css('md-card-actions > span > button'));
+            expect(els.length).toEqual(2);
+        });
+
+        it('should display a button with class with-icon if action has an icon and a text', () => {
+            component.actions = [
+                {text: 'Yes', 'type': 'info', action: () => {}, 'icon': 'randomIcon'}
+            ];
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-actions > span > button')).nativeElement;
+            expect(el.className).toEqual('with-icon');
+        });
+
+        it('should display a button without class if action has no icon and a text', () => {
+            component.actions = [
+                {text: 'Yes', 'type': 'info', action: () => {}, icon: null}
+            ];
+            component.ngOnInit = () => {}; // Prevent ngOnInit to set an icon from type
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-actions > span > button')).nativeElement;
+            expect(el.className).toBeFalsy();
+        });
+
+        it('should display a button md-mini-fab if action has an icon but no text', () => {
+            component.actions = [
+                {'type': 'info', action: () => {}, 'icon': 'randomIcon'}
+            ];
+            fixture.detectChanges();
+
+            const el = fixture.debugElement.query(By.css('md-card-actions > span > button')).nativeElement;
+            expect(el.attributes['md-mini-fab']).toBeTruthy();
+        });
+    });
+
+});
