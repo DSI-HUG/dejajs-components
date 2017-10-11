54fd13f86b4c9923a3eb71b3a4e541dd29bfd1b3
rtrompier <remy.trompier@gmail.com>
2017-10-11 15:14:01 +0200

    test(DejaSidenav): E2E test

--- e2e/sidenav/sidenav.e2e-spec.ts
+++ e2e/sidenav/sidenav.e2e-spec.ts
@@ -6,7 +6,7 @@
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
 
-import { by, element } from 'protractor';
+import { browser, by, element } from 'protractor';
 import { Util } from '../util/index';
 import { SidenavPage } from './sidenav.po';
 
@@ -18,17 +18,52 @@ describe('Sidenav component', () => {
         sidenav = new SidenavPage();
     });
 
-    it('should be visible by default', async () => {
+    it('should be visible by default', () => {
         sidenav.navigateTo();
+        expect(element(by.css('deja-sidenav-menu')).isPresent()).toBe(true);
         expect(element(by.css('deja-sidenav-menu')).isDisplayed()).toBe(true);
     });
 
-    it('should reduce on menu click', async () => {
+    it('should open on menu click', () => {
         sidenav.navigateTo();
-        Util.waitForElement('deja-sidenav-menu');
-        expect(element(by.css('deja-sidenav-menu md-list-item.active .mat-list-text')).isDisplayed()).toBe(true);
 
-        element(by.css('md-sidenav .header-menu-btn')).click();
-        expect(element(by.css('deja-sidenav-menu md-list-item.active .mat-list-text')).isDisplayed()).toBe(false);
+        Util.waitForElement('deja-sidenav-menu mat-list-item.active');
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active')).isPresent()).toBe(true);
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active .mat-list-text')).isDisplayed()).toBe(false, 'Should be close by default');
+
+        element(by.css('mat-sidenav .header-menu-btn')).click();
+        browser.sleep(500);
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active .mat-list-text')).isDisplayed()).toBe(true, 'Should be open');
+
+        element(by.css('mat-sidenav .header-menu-btn')).click();
+        browser.sleep(500);
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active .mat-list-text')).isDisplayed()).toBe(false, 'Should be close');
+    });
+
+    it('should be open by default on desktop', () => {
+        sidenav.navigateTo();
+        browser.driver.manage().window().setSize(1920, 1080);
+
+        Util.waitForElement('deja-sidenav-menu mat-list-item.active');
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active')).isPresent()).toBe(true);
+        expect(element(by.css('deja-sidenav-menu mat-list-item.active .mat-list-text')).isDisplayed()).toBe(true, 'Should be open');
+
+        browser.driver.manage().window().setSize(1024, 768);
+        browser.sleep(500);
+    });
+
+    it('should be hidden on mobile', () => {
+        sidenav.navigateTo();
+        browser.driver.manage().window().setSize(375, 667);
+
+        Util.waitForElement('deja-sidenav-menu');
+        expect(element(by.css('deja-sidenav-menu')).isDisplayed()).toBe(false, 'Should be hidden');
+
+        element(by.css('mat-toolbar .menu-btn')).click();
+        browser.sleep(500);
+        expect(element(by.css('deja-sidenav-menu')).isDisplayed()).toBe(true, 'Should be visible');
+
+        browser.driver.manage().window().setSize(1024, 768);
+        browser.sleep(500);
     });
 });
