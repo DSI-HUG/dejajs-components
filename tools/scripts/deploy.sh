63044f27b4b18a186a7546ad01e081dfd13a2a39
rtrompier <remy.trompier@gmail.com>
2017-10-13 14:52:03 +0200

    quiet(Test): test conf travis

---
+++ tools/scripts/deploy.sh
@@ -0,0 +1,26 @@
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+# Clean
+rm -rf dist-prod
+rm -rf docs
+
+# Run our compile script and deploy dist directory into th GH-PAGES branch
+demo="yarn run build:prod && node_modules/.bin/gh-pages -d dist-demo --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-components.git > demo.log"
+# Run our compile script and deploy docs directory into th GH-PAGES branch
+docs="yarn run docs:build && node_modules/.bin/gh-pages -d docs --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-doc.git > docs.log"
+# Deploy dist directory into th GH-PAGES branch
+test="node_modules/.bin/gh-pages -d coverage --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-test.git > coverage.log"
+
+node_modules/.bin/concurrently -p "[{name}]" -n "demo,docs,test" "$demo" "$docs" "$test"
+
+cat demo.log && rm demo.log
+cat docs.log && rm docs.log
+cat coverage.log && rm coverage.log
--- tools/scripts/validation.sh
+++ tools/scripts/validation.sh
@@ -1,29 +1,5 @@
 # #!/bin/bash
-# set -e # Exit with nonzero exit code if anything fails
-
-# SOURCE_BRANCH="master"
-
-# # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#     echo "Skipping deploy; just doing a build."
-#     exit 0
-# fi
-
-# # Clean
-# rm -rf dist-prod
-# rm -rf docs
-
-# # Run our compile script and deploy dist directory into th GH-PAGES branch
-# demo="yarn run build:prod && node_modules/.bin/gh-pages -d dist-demo --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-components.git"
-# # Run our compile script and deploy docs directory into th GH-PAGES branch
-# docs="yarn run docs:build && node_modules/.bin/gh-pages -d docs --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-doc.git"
-# # Deploy dist directory into th GH-PAGES branch
-# test="node_modules/.bin/gh-pages -d coverage --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-test.git"
-
-# command="node_modules/.bin/concurrently \"$demo\" \"$docs\" \"$test\""
-
-
-set -e
+set -e # Exit with nonzero exit code if anything fails
 
 license="yarn run licensecheck > license.log"
 lint="yarn run lint > lint.log"
