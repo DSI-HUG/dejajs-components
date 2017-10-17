9f7b5ba590e30a0ab4bf444b29fa076e170142d7
rtrompier <remy.trompier@gmail.com>
2017-10-17 14:51:57 +0200

    quiet(Travis): Show log if error

--- tools/scripts/validation.sh
+++ tools/scripts/validation.sh
@@ -8,9 +8,3 @@ e2e="yarn run e2e > e2e.log"
 lib="yarn run build:lib > lib.log"
 
 node_modules/.bin/concurrently -p "[{name}]" -n "license,lint,test,e2e,build:lib" "$license" "$lint" "$test" "$e2e" "$lib" > /dev/null
-
-cat license.log && rm license.log
-cat lint.log && rm lint.log
-cat test.log && rm test.log
-cat e2e.log && rm e2e.log
-cat lib.log && rm lib.log
