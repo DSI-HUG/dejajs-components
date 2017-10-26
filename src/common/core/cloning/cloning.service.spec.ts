4c0ded560b4e23b4735708d240ad1153f653c77f
Serge <dasilvasoa@gmail.com>
2017-10-26 12:45:50 +0200

    quiet(cloningService): Improve unit test

--- src/common/core/cloning/cloning.service.spec.ts
+++ src/common/core/cloning/cloning.service.spec.ts
@@ -11,6 +11,13 @@ import { CloningService } from './cloning.service';
 describe('CloningService', () => {
     let service: CloningService;
 
+    class MyTypedObject {
+        public datas: any;
+        constructor(d: any) {
+            this.datas = d;
+        }
+    }
+
     const datas = [
         {
             id: 2,
@@ -32,6 +39,11 @@ describe('CloningService', () => {
                     required: false
                 }
             ],
+            date: Date.now(),
+            item: {
+                name: 'Party',
+                color: '#333',
+            },
             title: 'Pizza Party'
         },
         {
@@ -57,6 +69,12 @@ describe('CloningService', () => {
                     required: true
                 }
             ],
+            date: new Date(1968, 5, 1, 10, 1, 56, 34),
+            regexp: new RegExp(/\test/, 'i'),
+            item: {
+                name: 'Bonanza',
+                color: '#245',
+            },
             title: 'Burger Bonanza'
         }
     ];
@@ -65,8 +83,43 @@ describe('CloningService', () => {
         service = new CloningService();
     });
 
-    it('Should, clone an complex structure', () => {
+    it('Should, clone a complex structure without target', () => {
         const cloned = service.cloneSync(datas);
         expect(JSON.stringify(cloned)).toEqual(JSON.stringify(datas));
+    });
+
+    it('Should, clone a complex structure with a target array', () => {
+        const cloned = [];
+        service.cloneSync(datas, cloned);
+        expect(JSON.stringify(cloned)).toEqual(JSON.stringify(datas));
+    });
+
+    it('Should, clone a complex structure with a target object', () => {
+        const cloned = {};
+        const d = { datas: datas };
+        service.cloneSync(d, cloned);
+        expect(JSON.stringify(cloned)).toEqual(JSON.stringify(d));
+    });
+
+    it('Should, clone a null value', () => {
+        const cloned = {};
+        expect(service.cloneSync(null, cloned)).toBeNull();
+    });
+
+    it('Should, clone a typed object', () => {
+        const d = new MyTypedObject(datas);
+        const cloned = service.cloneSync(d, MyTypedObject);
+        expect(JSON.stringify(cloned)).toEqual(JSON.stringify(d));
+        expect(cloned instanceof MyTypedObject).toBeTruthy();
+    });
+
+    it('Should, clone asynchronously', () => {
+        const d = new MyTypedObject(datas);
+        service.clone$(d, MyTypedObject)
+            .first()
+            .subscribe((cloned) => {
+                expect(JSON.stringify(cloned)).toEqual(JSON.stringify(d));
+                expect(cloned instanceof MyTypedObject).toBeTruthy();
+            });
     });
 });
