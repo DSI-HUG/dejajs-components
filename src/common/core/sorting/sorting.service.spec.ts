3c0bc8d165642315b5b8dc5f4e2f35a57caa1a4a
Serge <dasilvasoa@gmail.com>
2017-11-06 18:45:01 +0100

    quiet(SortingService): Improve unit test

--- src/common/core/sorting/sorting.service.spec.ts
+++ src/common/core/sorting/sorting.service.spec.ts
@@ -17,6 +17,7 @@ describe('SortingService', () => {
         {
             name: 'Apricots',
             value: 7,
+            date: new Date(2017, 12, 7),
             colors: [
                 'orange',
                 'yellow',
@@ -26,6 +27,7 @@ describe('SortingService', () => {
         {
             name: 'Banana',
             value: 5,
+            date: new Date(2017, 12, 5),
             colors: [
                 'green',
                 'yellow',
@@ -35,6 +37,7 @@ describe('SortingService', () => {
         {
             name: 'Cantaloupe',
             value: 4,
+            date: new Date(2017, 12, 4),
             colors: [
                 'orange',
                 'cantaloupe',
@@ -44,6 +47,7 @@ describe('SortingService', () => {
         {
             name: 'Cherries',
             value: 6,
+            date: new Date(2017, 12, 6),
             colors: [
                 'mallow',
                 'red',
@@ -52,6 +56,7 @@ describe('SortingService', () => {
         {
             name: 'Coconut',
             value: 2,
+            date: new Date(2017, 12, 2),
             colors: [
                 'white',
                 'braun',
@@ -61,6 +66,7 @@ describe('SortingService', () => {
         {
             name: 'Cranberries',
             value: 8,
+            date: new Date(2017, 12, 8),
             colors: [
                 'mallow',
                 'black',
@@ -70,6 +76,7 @@ describe('SortingService', () => {
         {
             name: 'Durian',
             value: 1,
+            date: new Date(2017, 12, 1),
             colors: [
                 'green',
                 'yellow',
@@ -78,6 +85,7 @@ describe('SortingService', () => {
         {
             name: 'Grapes',
             value: 3,
+            date: new Date(2017, 12, 3),
             colors: [
                 'green',
                 'red',
@@ -86,7 +94,8 @@ describe('SortingService', () => {
         },
         {
             name: 'Lemon',
-            value: 12,
+            value: 0,
+            date: new Date(2017, 12, 0),
             colors: [
                 'yellow',
                 'green',
@@ -95,6 +104,7 @@ describe('SortingService', () => {
         {
             name: 'Mango',
             value: 9,
+            date: new Date(2017, 12, 9),
             colors: [
                 'yellow',
                 'green',
@@ -105,6 +115,7 @@ describe('SortingService', () => {
         {
             name: 'Pineapple',
             value: 11,
+            date: new Date(2017, 12, 11),
             colors: [
                 'yellow',
             ],
@@ -112,6 +123,7 @@ describe('SortingService', () => {
         {
             name: 'Watermelon',
             value: 10,
+            date: new Date(2017, 12, 10),
             colors: [
                 'green',
                 'red',
@@ -160,6 +172,34 @@ describe('SortingService', () => {
             });
     });
 
+    it('Should, sort by number', () => {
+        const si = {
+            name: 'value',
+            order: SortOrder.ascending,
+        } as ISortInfos;
+
+        service.sort$(fructs, si)
+            .first()
+            .subscribe((sorted) => {
+                expect(sorted.length).toBe(fructs.length);
+                sorted.forEach((value, i) => expect(value.value).toEqual(i));
+            });
+    });
+
+    it('Should, sort by date', () => {
+        const si = {
+            name: 'date',
+            order: SortOrder.ascending,
+        } as ISortInfos;
+
+        service.sort$(fructs, si)
+            .first()
+            .subscribe((sorted) => {
+                expect(sorted.length).toBe(fructs.length);
+                sorted.forEach((value, i) => expect(value.value).toEqual(i));
+            });
+    });
+
     it('Should, sort a tree', () => {
         const si = [{
             name: 'name',
@@ -183,14 +223,52 @@ describe('SortingService', () => {
     });
 
     // Compare static method test
-    it('Should, compare correctly', () => {
+    it('Should, compare values correctly', () => {
         const si = {
-            name: 'name',
             order: SortOrder.ascending,
-        };
+        } as ISortInfos;
 
         expect(SortingService.compare(null, undefined, si)).toBe(0);
         expect(SortingService.compare(null, 0, si)).toBe(-1);
         expect(SortingService.compare(0, undefined, si)).toBe(1);
+
+        si.name = 'name';
+        expect(SortingService.compare({}, {}, si)).toBe(0);
+        expect(SortingService.compare({}, { name: '' }, si)).toBe(-1);
+        expect(SortingService.compare({ name: '' }, {}, si)).toBe(1);
+
+        expect(SortingService.compare({ name: 'b' }, { name: 'b' }, si)).toBe(0);
+        expect(SortingService.compare({ name: 'b' }, { name: 'a' }, si)).toBe(1);
+        expect(SortingService.compare({ name: 'a' }, { name: 'b' }, si)).toBe(-1);
+
+        expect(SortingService.compare({ name: () => 'a' }, { name: () => 'a' }, si)).toBe(0);
+        expect(SortingService.compare({ name: () => 'b' }, { name: () => 'a' }, si)).toBe(1);
+        expect(SortingService.compare({ name: () => 'a' }, { name: () => 'b' }, si)).toBe(-1);
+        expect(SortingService.compare({ name: () => 'a' }, { name: () => '' }, si)).toBe(1);
+        expect(SortingService.compare({ name: () => '' }, { name: () => 'a' }, si)).toBe(-1);
+
+        // tslint:disable-next-line:no-construct
+        expect(SortingService.compare({ name: () => new String('b') }, { name: () => new String('a') }, si)).toBe(1);
+        // tslint:disable-next-line:no-construct
+        expect(SortingService.compare({ name: () => new String('a') }, { name: () => new String('b') }, si)).toBe(-1);
+    });
+
+    it('Should, compare dates correctly', () => {
+        const si = {
+            name: 'name',
+            order: SortOrder.ascending,
+        } as ISortInfos;
+
+        const date1 = new Date(2017, 12, 25, 23, 59, 58);
+        const date2 = date1;
+        const date3 = date2.setSeconds(57);
+
+        expect(SortingService.compare({ name: date1 }, { name: date2 }, si)).toBe(0);
+        expect(SortingService.compare({ name: date1 }, { name: date3 }, si)).toBe(1);
+        expect(SortingService.compare({ name: date3 }, { name: date1 }, si)).toBe(-1);
+
+        expect(SortingService.compare({ name: () => date1 }, { name: () => date2 }, si)).toBe(0);
+        expect(SortingService.compare({ name: () => date1 }, { name: () => date3 }, si)).toBe(1);
+        expect(SortingService.compare({ name: () => date3 }, { name: () => date1 }, si)).toBe(-1);
     });
 });
