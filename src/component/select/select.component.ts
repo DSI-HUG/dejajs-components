14efbbde2140745ea1e62f5163645d908f63255d
Serge <dasilvasoa@gmail.com>
2017-11-23 09:32:34 +0100

    fix(DejaSelect): Autocomplete clear model when text is changed DEJS-246

--- src/component/select/select.component.ts
+++ src/component/select/select.component.ts
@@ -1058,7 +1058,9 @@ export class DejaSelectComponent extends ItemListBase implements ControlValueAcc
             // Autocomplete or multiselect only
             this.dropDownQuery = this.query;
             if (this.isModeAutocomplete) {
-                this.unselectAll$().first().subscribe(noop);
+                this.unselectAll$()
+                    .first()
+                    .subscribe(() => this.onModelChange());
             }
         }
     }
