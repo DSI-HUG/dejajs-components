2f81d0492a0fcacd32e1c2336cee48d4a96506a8
rtrompier <remy.trompier@gmail.com>
2017-12-12 17:24:25 +0100

    quiet(TimeAgoPipe): Fix name and fit

--- src/common/core/pipes/time-ago.pipe.spec.ts
+++ src/common/core/pipes/time-ago.pipe.spec.ts
@@ -11,25 +11,25 @@ import { TimeAgoPipe } from './time-ago.pipe';
 import * as moment_ from 'moment';
 const moment: (value?: any, format?: string) => moment_.Moment = (<any>moment_).default || moment_;
 
-describe('DejaChipsComponent', () => {
+describe('TimeAgoPipe', () => {
     const pipe = new TimeAgoPipe(null, null);
 
     it('should create the pipe intance', () => {
         expect(pipe).toBeTruthy();
     });
 
-    fit('should be a few seconds ago', async () => {
+    it('should be a few seconds ago', async () => {
         const value = pipe.transform(new Date());
         expect(value).toEqual('a few seconds ago');
     });
 
-    fit('should be in 30 minutes', async () => {
+    it('should be in 30 minutes', async () => {
         const date = moment().add(30, 'minutes');
         const value = pipe.transform(date);
         expect(value).toEqual('in 30 minutes');
     });
 
-    fit('should be in 120 minutes', async () => {
+    it('should be in 120 minutes', async () => {
         const date = moment().add(120, 'minutes');
         const value = pipe.transform(date);
         expect(value).toEqual('in 2 hours');
