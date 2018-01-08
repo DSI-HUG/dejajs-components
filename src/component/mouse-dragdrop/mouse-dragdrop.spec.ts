d72348cbeed05eeb56eb690ffbef85c89ecce398
Serge <dasilvasoa@gmail.com>
2018-01-08 11:36:14 +0100

    test(DejaMouseDragDrop): Improve unit test

--- src/component/mouse-dragdrop/mouse-dragdrop.spec.ts
+++ src/component/mouse-dragdrop/mouse-dragdrop.spec.ts
@@ -7,7 +7,7 @@
  */
 
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -19,6 +19,7 @@ import { DejaMouseDraggableDirective, IDejaMouseDraggableContext } from './mouse
 import { DejaMouseDroppableDirective, IDejaMouseDroppableContext } from './mouse-droppable.directive';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
     template: `<div id="dragArea" [deja-mouse-draggable]="getDragContext()">DragAndDroppedContent</div>
                 <div #dropArea id="dropArea" [deja-mouse-droppable]="getDropContext(dropArea)"></div>
@@ -43,7 +44,7 @@ import { DejaMouseDroppableDirective, IDejaMouseDroppableContext } from './mouse
     `]
 })
 class DejaMouseDragDropComponent {
-    protected getDragContext() {
+    public getDragContext() {
         return {
             className: 'drag-cursor',
             dragStart: (target: HTMLElement) => {
@@ -52,7 +53,7 @@ class DejaMouseDragDropComponent {
         } as IDejaMouseDraggableContext;
     }
 
-    protected getDropContext(dropArea: HTMLElement) {
+    public getDropContext(dropArea: HTMLElement) {
         return {
             dragEnter: (_dragContext) => {
                 return {
@@ -107,60 +108,48 @@ describe('DejaMouseDragDrop', () => {
 
     it('should be able to drag and drop from the first div to the second', async(() => {
         const fixture = TestBed.createComponent(DejaMouseDragDropComponent);
-        const dragDropContainerElement = fixture.nativeElement as HTMLElement;
 
-        const sendMouseEvent = (element: EventTarget, type: string, x: number, y: number, buttons = 0) => {
-            const dragDropContainerBounds = dragDropContainerElement.getBoundingClientRect();
-            const eventInit = () => ({
-                bubbles: true,
-                cancelable: (type !== 'mousemove'),
-                view: document.defaultView,
-                altKey: false,
-                ctrlKey: false,
-                metaKey: false,
-                shiftKey: false,
-                button: 0,
-                buttons: buttons,
-                clientX: dragDropContainerBounds.left + x,
-                clientY: dragDropContainerBounds.top + y,
-                relatedTarget: element,
-            } as MouseEventInit);
-            const event = new MouseEvent(type, eventInit());
-            element.dispatchEvent(event);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            const dragDebugElement = fixture.debugElement.query(By.css('div#dragArea'));
+            const dropDebugElement = fixture.debugElement.query(By.css('div#dropArea'));
+            const dragDropContainerElement = fixture.nativeElement as HTMLElement;
+            const dragElement = dragDebugElement.nativeElement as HTMLElement;
+
+            const sendMouseEvent = (element: EventTarget, type: string, x: number, y: number, buttons = 0) => {
+                const dragDropContainerBounds = dragDropContainerElement.getBoundingClientRect();
+                const eventInit = () => ({
+                    bubbles: true,
+                    cancelable: (type !== 'mousemove'),
+                    view: document.defaultView,
+                    altKey: false,
+                    ctrlKey: false,
+                    metaKey: false,
+                    shiftKey: false,
+                    button: 0,
+                    buttons: buttons,
+                    clientX: dragDropContainerBounds.left + x,
+                    clientY: dragDropContainerBounds.top + y,
+                    relatedTarget: element,
+                } as MouseEventInit);
+                const event = new MouseEvent(type, eventInit());
+                element.dispatchEvent(event);
+            };
+
+            sendMouseEvent(dragElement, 'mouseenter', 101, 101);
+            sendMouseEvent(dragElement, 'mousemove', 200, 200);
+            sendMouseEvent(dragElement, 'mousedown', 200, 200, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mousemove', 220, 220, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mousemove', 200, 400, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mousemove', 400, 400, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mousemove', 250, 400, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mousemove', 200, 400, 1);
+            sendMouseEvent(dragElement.ownerDocument, 'mouseup', 200, 400, 0);
+
             fixture.detectChanges();
-        };
+            const dropElement = dropDebugElement.nativeElement as HTMLElement;
 
-        Observable.fromPromise(fixture.whenStable())
-            .first()
-            .map(() => {
-                const dragDebugElement = fixture.debugElement.query(By.css('div#dragArea'));
-                expect(dragDebugElement).toBeDefined();
-                return dragDebugElement.nativeElement;
-            })
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement, 'mouseenter', 101, 101))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement, 'mousemove', 200, 200))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement, 'mousedown', 200, 200, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mousemove', 220, 220, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mousemove', 200, 400, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mousemove', 400, 400, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mousemove', 250, 400, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mousemove', 200, 400, 1))
-            .delay(1)
-            .do((dragElement: HTMLElement) => sendMouseEvent(dragElement.ownerDocument, 'mouseup', 200, 400, 0))
-            .delay(1)
-            .subscribe((dragElement: HTMLElement) => {
-                const dropDebugElement = fixture.debugElement.query(By.css('div#dropArea'));
-                const dropElement = dropDebugElement.nativeElement as HTMLElement;
-                expect(dropElement.innerText).toEqual(dragElement.innerText);
-            });
-
+            expect(dropElement.innerText).toEqual(dragElement.innerText);
+        });
     }));
 });
