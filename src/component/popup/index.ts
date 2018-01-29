6b26d421e66fddac58d3c4b3ffd60b0c185d0544
Christian Casamirra <casc99casc@gmail.com>
2018-01-29 16:53:54 +0100

    refactor(Popup): added license

--- src/component/popup/index.ts
+++ src/component/popup/index.ts
@@ -1,3 +1,10 @@
+/*
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
 import { PortalModule } from '@angular/cdk/portal';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
