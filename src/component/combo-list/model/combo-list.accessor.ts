412d92ebe194d491b6921910ea017f50d21cc731
Christian Casamirra <casc99casc@gmail.com>
2018-02-13 20:04:27 +0100

    fix(DejaComboList): aot - no lambda functions in decorators.

--- src/component/combo-list/model/combo-list.accessor.ts
+++ src/component/combo-list/model/combo-list.accessor.ts
@@ -3,10 +3,11 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export const noop = () => { };
 
-export const valueAccessorFactory = (component: any) => {
+// tslint:disable-next-line:only-arrow-functions
+export function valueAccessorFactory(component: any) {
     return {
         provide: NG_VALUE_ACCESSOR,
         useExisting: forwardRef(() => component),
         multi: true
     };
-};
+}
