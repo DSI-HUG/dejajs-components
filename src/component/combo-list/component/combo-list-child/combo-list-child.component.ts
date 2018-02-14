2d86408963985f93c29b8f9fd14b744cefb81aed
Christian Casamirra <casc99casc@gmail.com>
2018-02-14 08:33:06 +0100

    fix(DejaComboList): license

--- src/component/combo-list/component/combo-list-child/combo-list-child.component.ts
+++ src/component/combo-list/component/combo-list-child/combo-list-child.component.ts
@@ -1,10 +1,10 @@
 /*
-*  @license
-*  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
-*
-*  Use of this source code is governed by an Apache-2.0 license that can be
-*  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
-*/
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
 import {
     Component,
     EventEmitter,
