91fef809a0fec949b3daf1623749edc52531daf4
Serge <dasilvasoa@gmail.com>
2018-03-02 15:16:12 +0100

    quiet(Deploy): Config for demo, doc and coverage

--- tools/scripts/deploy.sh
+++ tools/scripts/deploy.sh
@@ -8,19 +8,16 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-# Clean
-rm -rf dist-prod
-rm -rf doc
-
 # Run our compile script and deploy dist directory into th GH-PAGES branch
-demo="gulp build:demo && node_modules/.bin/gh-pages -d demo/dist --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-components.git > demo.log"
+rm -rf demo/dist
+gulp build:demo
+node_modules/.bin/gh-pages -d demo/dist --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-components.git
+
 # Run our compile script and deploy doc directory into th GH-PAGES branch
-doc="gulp build:doc && node_modules/.bin/gh-pages -d doc --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-doc.git > doc.log"
+rm -rf doc
+gulp build:doc
+node_modules/.bin/gh-pages -d doc --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-doc.git
+
 # Deploy dist directory into th GH-PAGES branch
-test="node_modules/.bin/gh-pages -d coverage --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-test.git > coverage.log"
-
-node_modules/.bin/concurrently -p "[{name}]" -n "demo,doc,test" "$demo" "$doc" "$test"
-
-cat demo.log && rm demo.log
-cat doc.log && rm doc.log
-cat coverage.log && rm coverage.log
+rm -rf coverage
+node_modules/.bin/gh-pages -d coverage --repo https://rtrompier:${GITHUB_TOKEN}@github.com/DSI-HUG/dejajs-test.git
