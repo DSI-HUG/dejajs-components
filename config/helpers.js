60ad4d41326e16e96458549817f22fa75d387d15
Serge <dasilvasoa@gmail.com>
2018-03-13 23:21:03 +0100

    fix(gulp): Fix install sequence with npm

--- config/helpers.js
+++ config/helpers.js
@@ -4,13 +4,12 @@ const exec = require('child_process').exec;
 
 const _root = path.resolve(__dirname, '..');
 
-
 /**
  * Plaform independant path to an executable cmd
  * @param {string} path
  */
 platformPath = (path) => {
-  return /^win/.test(os.platform()) ? `${path}.cmd` : path;
+    return /^win/.test(os.platform()) ? `${path}.cmd` : path;
 };
 
 /**
@@ -18,7 +17,7 @@ platformPath = (path) => {
  * @param {string[]} args
  */
 rootDir = (...args) => {
-  return path.join.apply(path, [_root].concat(...args));
+    return path.join.apply(path, [_root].concat(...args));
 };
 
 /**
@@ -26,7 +25,7 @@ rootDir = (...args) => {
  * @param {string} cmd
  */
 binPath = (cmd) => {
-  return platformPath(`/node_modules/.bin/${cmd}`);
+    return platformPath(`/node_modules/.bin/${cmd}`);
 };
 
 /**
@@ -37,37 +36,26 @@ binPath = (cmd) => {
  * @returns {Promise<number>}
  */
 execp = (cmd, opts) => {
-  opts = Object.assign(opts || {}, {
-    stdout: process.stdout,
-    stderr: process.stderr
-  });
-  return new Promise((resolve, reject) => {
-    const child = exec(cmd, opts,
-      (err, stdout, stderr) => err ? reject(err.code) : resolve(0));
+    opts = Object.assign(opts || {}, {
+        stdout: process.stdout,
+        stderr: process.stderr
+    });
+    return new Promise((resolve, reject) => {
+        const child = exec(cmd, opts,
+            (err, stdout, stderr) => err ? reject(err.code) : resolve(0));
 
-    if (opts.stdout) {
-      child.stdout.pipe(opts.stdout);
-    }
-    if (opts.stderr) {
-      child.stderr.pipe(opts.stderr);
-    }
-  });
-};
-
-/**
- * Install dependencies using yarn, if present, or npm otherwise.
- * @param opts See child_process.exec node docs
- * @returns {Promise<number>}
- */
-installDependencies = (opts) => {
-  return execp('yarn -v') // first try to install deps using yarn
-    .then(exitCode => exitCode === 0 ? execp('yarn install', opts) : execp('npm install', opts));
+        if (opts.stdout) {
+            child.stdout.pipe(opts.stdout);
+        }
+        if (opts.stderr) {
+            child.stderr.pipe(opts.stderr);
+        }
+    });
 };
 
 var exports = module.exports = {
-  root: rootDir,
-  execp: execp,
-  binPath: binPath,
-  platformPath: platformPath,
-  installDependencies: installDependencies
+    root: rootDir,
+    execp: execp,
+    binPath: binPath,
+    platformPath: platformPath
 };
