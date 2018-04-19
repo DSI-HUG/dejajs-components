92249f5c88f3701a227ff138ccbcb6df7b3198b8
Serge <dasilvasoa@gmail.com>
2018-04-19 12:43:11 +0200

    quiet(Global): Updating dependencies for Webpack V4

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -28,7 +28,7 @@ const getConfig = (hasCoverage, isTddMode) => {
 
     return {
         devtool: 'inline-source-map',
-        // mode: 'development', // For webpack V4 
+        mode: 'production',
         resolve: {
             extensions: ['.ts', '.js'],
             modules: [helpers.root('src'), 'node_modules']
