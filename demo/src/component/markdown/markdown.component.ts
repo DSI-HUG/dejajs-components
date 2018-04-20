7edb99f2d49ee4c4177b4a509129f9fa95ace0bc
DETURCHE Cédric <Cedric.Deturche@hcuge.ch>
2018-04-20 14:06:08 +0200

    Fix test with HttpClient (DEJS-406)

--- demo/src/component/markdown/markdown.component.ts
+++ demo/src/component/markdown/markdown.component.ts
@@ -43,6 +43,8 @@ export class DejaMarkdownComponent implements OnInit, AfterViewChecked {
         this._http.get(url, { observe: 'body', headers: headers, responseType: 'text' }).subscribe((object) => {
             this.value = object.toString();
         }, (error) => {
+            console.log('Error');
+            console.log(error);
             this.value = `${error.message}`;
         });
     }
