dbcfba1caab93e5609590229fa4e6a4be5c4ccfd
Serge <vapkse@gmail.com>
2018-09-05 14:09:52 +0200

    quiet(Gulp): Console Message

--- gulpfile.js
+++ gulpfile.js
@@ -673,7 +673,7 @@ gulp.task('push-changes', (cb) => {
 
 gulp.task('create-new-tag', (cb) => {
 	let version = `v${getPackageJsonVersion()}`;
-	gulpGit.tag(version, `chore(release): :sparkles: :tada: create tag for version v${version}`, cb);
+	gulpGit.tag(version, `chore(release): :sparkles: :tada: create tag for version ${version}`, cb);
 });
 
 gulp.task('release', gulp.series('bump-version', 'changelog', 'commit-changes', 'create-new-tag', 'push-changes'));
