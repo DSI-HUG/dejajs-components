017fb9ec23c9f3681577f8472c4c36f920cd0348
rtrm <rtrm@hcuge.ch>
2018-09-24 10:50:42 +0200

    fix(DejaSidenav): Add BrowserAnimationsModule for test

--- src/component/sidenav/sidenav.component.spec.ts
+++ src/component/sidenav/sidenav.component.spec.ts
@@ -8,6 +8,7 @@
 
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DejaSidenavModule } from './index';
 import { DejaSidenavComponent } from './sidenav.component';
@@ -21,6 +22,7 @@ describe('DejaSidenavComponent', () => {
         TestBed.configureTestingModule({
             imports: [
                 RouterTestingModule,
+                BrowserAnimationsModule,
                 DejaSidenavModule.forRoot(),
             ],
             schemas: [NO_ERRORS_SCHEMA],
