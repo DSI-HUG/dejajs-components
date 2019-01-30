82e77df36b4865009b9037773198837ecf1c2d5e
bchn <benjamin.champion@hcuge.ch>
2019-01-30 14:57:53 +0100

    fix(TileGroup): Fix import

--- src/component/tiles/tile-group.class.ts
+++ src/component/tiles/tile-group.class.ts
@@ -6,7 +6,7 @@
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
 
-import { Rect } from '../../common';
+import { Rect } from '../../common/core/graphics/rect';
 import { DejaTile } from './tile.class';
 
 export enum DejaTileBorderDirection {
