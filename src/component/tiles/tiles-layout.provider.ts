6d33fc84c9db0da222886008502204328ccd5035
Serge <vapkse@gmail.com>
2019-06-07 10:32:26 +0200

    fix(TilesComponent): Fix container auto height

--- src/component/tiles/tiles-layout.provider.ts
+++ src/component/tiles/tiles-layout.provider.ts
@@ -146,6 +146,9 @@ export class DejaTilesLayoutProvider implements OnDestroy {
                     if (tile.isSelected && !tile.isHidden) {
                         selectedTileIds.push(tile.id);
                     }
+                    if (tile.pixelBounds.bottom > height) {
+                        height = tile.pixelBounds.bottom;
+                    }
                 });
 
                 let top = maxHeight;
