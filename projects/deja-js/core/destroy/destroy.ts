4e01ac86c5bb3751a6c1d26ebebdec47446926c7
Serge <vapkse@gmail.com>
2020-02-04 18:00:16 +0100

    fix(destroy.ts): Add licence

--- projects/deja-js/core/destroy/destroy.ts
+++ projects/deja-js/core/destroy/destroy.ts
@@ -1,3 +1,19 @@
+/*
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
+
+/**
+ * Remove diacritics (accent and other marks) on characters, and dissociate double characters.
+ * Based on the character map of http://lehelk.com/2011/05/06/script-to-remove-diacritics/
+ * but per-character walk (improved performance).
+ *
+ * Licensed under WTFPL v2 http://sam.zoy.org/wtfpl/COPYING
+ */
+
 import { OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 
