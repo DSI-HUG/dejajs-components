e46bedc8007706cfe0cae653d7233726ca5d14dd
Serge <dasilvasoa@gmail.com>
2020-02-12 22:29:49 +0100

    quiet(Index): Fix License position

--- projects/deja-js/component/index.ts
+++ projects/deja-js/component/index.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 /*
- *  @license
- *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
- *
- *  Use of this source code is governed by an Apache-2.0 license that can be
- *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
- */
+*  @license
+*  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+*
+*  Use of this source code is governed by an Apache-2.0 license that can be
+*  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+*/
+
+import { NgModule } from '@angular/core';
 
 @NgModule({})
 export class DejaJsComponentModule { }
