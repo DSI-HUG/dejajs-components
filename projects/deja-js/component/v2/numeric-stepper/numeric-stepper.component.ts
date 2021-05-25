bd92c31079f4d203793fabb9910f7262a6281eb8
Serge <vapkse@gmail.com>
2021-05-25 15:34:29 +0200

    fix(): fix shadow style

--- projects/deja-js/component/v2/numeric-stepper/numeric-stepper.component.ts
+++ projects/deja-js/component/v2/numeric-stepper/numeric-stepper.component.ts
@@ -7,9 +7,9 @@
  */
 
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
-import { Destroy } from '@deja-js/component/core';
-import { timer } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Destroy, KeyCodes } from '@deja-js/component/core';
+import { fromEvent, timer } from 'rxjs';
+import { delay, map, shareReplay, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -27,6 +27,7 @@ export class DejaNumericStepperComponent extends Destroy implements OnInit {
     public width: number = null;
     public topUp: number = null;
     public topDown: number = null;
+    public height: number = null;
 
     public constructor(
         private elementRef: ElementRef<HTMLElement>,
@@ -36,32 +37,62 @@ export class DejaNumericStepperComponent extends Destroy implements OnInit {
     }
 
     public ngOnInit(): void {
-        timer(100).pipe(
-            takeUntil(this.destroyed$)
-        ).subscribe(() => {
-            // Find form field
-            const parentElement = this.elementRef.nativeElement.parentElement;
-            let formFieldElement = parentElement;
-            // eslint-disable-next-line no-loops/no-loops
-            while (formFieldElement) {
-                if (formFieldElement.tagName === 'MAT-FORM-FIELD') {
-                    break;
+        const formFieldElement$ = timer(100).pipe(
+            map(() => {
+                // Find form field
+                const parentElement = this.elementRef.nativeElement.parentElement;
+                let formFieldElement = parentElement;
+                // eslint-disable-next-line no-loops/no-loops
+                while (formFieldElement) {
+                    if (formFieldElement.tagName === 'MAT-FORM-FIELD') {
+                        break;
+                    }
+                    formFieldElement = formFieldElement.parentElement;
                 }
-                formFieldElement = formFieldElement.parentElement;
-            }
 
-            if (formFieldElement) {
-                formFieldElement.setAttribute('deja-numeric-stepper-form-field', '');
-            }
+                if (formFieldElement) {
+                    formFieldElement.setAttribute('deja-numeric-stepper-form-field', '');
+                }
 
-            const formFieldBounds = formFieldElement.getBoundingClientRect();
-            const bounds = this.elementRef.nativeElement.getBoundingClientRect();
+                return formFieldElement;
+            }),
+            shareReplay(1)
+        );
 
-            this.left = formFieldBounds.left - bounds.left;
-            this.topUp = formFieldBounds.top - bounds.top - 28;
-            this.topDown = formFieldBounds.bottom - bounds.top - 10;
-            this.width = formFieldBounds.width;
-            this.changeDetectorRef.markForCheck();
-        });
+        formFieldElement$.pipe(
+            switchMap(formFieldElement => fromEvent<MouseEvent>(formFieldElement, 'mouseenter').pipe(
+                tap(() => {
+                    const formFieldBounds = formFieldElement.getBoundingClientRect();
+                    const bounds = this.elementRef.nativeElement.getBoundingClientRect();
+
+                    this.left = formFieldBounds.left - bounds.left;
+                    this.topUp = formFieldBounds.top - bounds.top - 28;
+                    this.topDown = formFieldBounds.bottom - bounds.top - 10;
+                    this.width = formFieldBounds.width;
+                    this.height = this.topDown - this.topUp + 32;
+                    this.changeDetectorRef.markForCheck();
+                })
+            )),
+            takeUntil(this.destroyed$)
+        ).subscribe();
+
+        formFieldElement$.pipe(
+            switchMap(formFieldElement => fromEvent<KeyboardEvent>(formFieldElement, 'keydown').pipe(
+                map(event => {
+                    if (event.code === KeyCodes.UpArrow) {
+                        this.increment.emit();
+                        event.preventDefault();
+                        return false;
+                    } else if (event.code === KeyCodes.DownArrow) {
+                        this.decrement.emit();
+                        event.preventDefault();
+                        return false;
+                    }
+
+                    return undefined;
+                })
+            )),
+            takeUntil(this.destroyed$)
+        ).subscribe();
     }
 }
