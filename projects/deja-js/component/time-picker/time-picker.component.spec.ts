ad950ba610de1bcb313202ca5556304ea452fa98
DUC Joffrey <joffrey.jo@hcuge.ch>
2021-06-02 14:07:09 +0200

    test(time-picker): complete time picker test

--- projects/deja-js/component/time-picker/time-picker.component.spec.ts
+++ projects/deja-js/component/time-picker/time-picker.component.spec.ts
@@ -6,33 +6,68 @@
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
 
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DejaNumericStepperModule } from '@deja-js/component/v2/numeric-stepper';
+import { timer } from 'rxjs';
 
-import { DejaTimePickerModule } from '.';
-import { DejaTimePickerComponent } from './time-picker.component';
+import { DejaTimePickerComponent, DejaTimePickerModule } from '.';
+
+@Component({
+    selector: 'DejaTimePickerContainerComponent',
+    template: `<form [formGroup]="dateForm">
+                <deja-time-picker formControlName="dateValue" [step]="5"></deja-time-picker>
+                </form>`
+})
+class DejaTimePickerContainerComponent {
+    public dateValue = new Date(2021, 4, 12, 9, 55);
+    public dateForm: FormGroup;
+
+    public constructor(private fb: FormBuilder) {
+        this.dateForm = this.fb.group({
+            dateValue: this.dateValue
+        });
+    }
+}
 
 describe('DejaTimePickerComponent', () => {
     let component: DejaTimePickerComponent;
-    let fixture: ComponentFixture<DejaTimePickerComponent>;
+    let containerComponent: DejaTimePickerContainerComponent;
+    let fixture: ComponentFixture<DejaTimePickerContainerComponent>;
 
     beforeEach(waitForAsync(() => {
         void TestBed.configureTestingModule({
+            declarations: [
+                DejaTimePickerContainerComponent
+            ],
             imports: [
                 BrowserAnimationsModule,
-                DejaTimePickerModule
-            ]
+                CommonModule,
+                DejaTimePickerModule,
+                FormsModule,
+                ReactiveFormsModule,
+                DejaNumericStepperModule,
+                MatFormFieldModule,
+                MatInputModule
+            ],
+            providers: [FormBuilder]
         }).compileComponents();
 
-        fixture = TestBed.createComponent(DejaTimePickerComponent);
-        component = fixture.componentInstance;
-        component.value = new Date(2021, 4, 12, 9, 55);
+        fixture = TestBed.createComponent(DejaTimePickerContainerComponent);
+        component = fixture.debugElement.query(By.directive(DejaTimePickerComponent)).componentInstance as DejaTimePickerComponent;
+        containerComponent = fixture.debugElement.componentInstance;
         fixture.detectChanges();
     }));
 
     it('should create the component', () => {
         void expect(component).toBeTruthy();
+        void expect(containerComponent).toBeTruthy();
     });
 
     it('should display a specific date', () => {
@@ -45,38 +80,26 @@ describe('DejaTimePickerComponent', () => {
         void expect(minutesElement.value).toEqual('55');
     });
 
-    // it('should increment the hours', () => {
-    //     const hoursIncrementArrowElement = fixture.debugElement.query(By.css('.time-picker > .hours > .numeric-stepper > .arrows.increment')).nativeElement as HTMLElement;
-    //     const hoursElement = fixture.debugElement.query(By.css('.time-picker > .hours > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
-    //     const minutesElement = fixture.debugElement.query(By.css('.time-picker > .minutes > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
+    it('should update the inputs', () => {
+        const hoursInputElement = fixture.debugElement.query(By.css('.time-picker > .hours input')).nativeElement as HTMLInputElement;
+        const minutesInputElement = fixture.debugElement.query(By.css('.time-picker > .minutes input')).nativeElement as HTMLInputElement;
 
-    //     hoursIncrementArrowElement.click();
-    //     fixture.detectChanges();
+        containerComponent.dateForm.controls.dateValue.patchValue(new Date(2021, 4, 12, 10, 55));
 
-    //     void expect(hoursElement.value).toEqual('10');
-    //     void expect(minutesElement.value).toEqual('55');
-    // });
+        fixture.detectChanges();
 
-    // it('should increment the minutes', () => {
-    //     const minutesIncrementArrowElement = fixture.debugElement.query(By.css('.time-picker > .minutes > .numeric-stepper > .arrows.increment')).nativeElement as HTMLElement;
-    //     const hoursElement = fixture.debugElement.query(By.css('.time-picker > .hours > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
-    //     const minutesElement = fixture.debugElement.query(By.css('.time-picker > .minutes > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
+        timer(1).subscribe(() => {
+            void expect(hoursInputElement.value).toEqual('10');
+            void expect(minutesInputElement.value).toEqual('55');
+        });
+    });
 
-    //     minutesIncrementArrowElement.click();
-    //     fixture.detectChanges();
+    it('should disable both hours and minutes mat form fields', () => {
+        const hoursMinutesFields = fixture.debugElement.query(By.css('.time-picker mat-form-field')).nativeElement as HTMLInputElement;
+        component.disabled = true;
 
-    //     void expect(hoursElement.value).toEqual('09');
-    //     void expect(minutesElement.value).toEqual('56');
-    // });
+        fixture.detectChanges();
 
-    // it('should be disabled', () => {
-    //     component.disabled = true;
-    //     fixture.detectChanges();
-
-    //     const hoursElement = fixture.debugElement.query(By.css('.time-picker > .hours > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
-    //     const minutesElement = fixture.debugElement.query(By.css('.time-picker > .minutes > .numeric-stepper > input.value')).nativeElement as HTMLInputElement;
-
-    //     void expect(hoursElement.getAttribute('disabled')).toEqual('');
-    //     void expect(minutesElement.getAttribute('disabled')).toEqual('');
-    // });
+        void expect(hoursMinutesFields.className).toContain('disabled');
+    });
 });
