f947419b797617c73af112642e8650f17f7cdcb6
Serge <vapkse@gmail.com>
2021-06-29 09:19:35 +0200

    feat(): try to simplify material date time picker demo

--- src/app/date-picker-material/date-picker-material-demo.ts
+++ src/app/date-picker-material/date-picker-material-demo.ts
@@ -7,12 +7,10 @@
  */
 
 import { Component, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDatepicker, MatDatepickerInput, MatDatepickerInputEvent, MatDateRangeInput, MatDateRangePicker } from '@angular/material/datepicker';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Destroy } from '@deja-js/component/core';
-import { isMatch, parse, set, startOfToday } from 'date-fns';
-
-import { dateTimeFormat } from './custom-date-format.directive';
+import { combineLatest, merge, of } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'dejadate-picker-material-demo',
@@ -42,82 +40,45 @@ export class DejaDatePickerMaterialDemoComponent extends Destroy {
         });
 
         this.dateTimeForm = this.fb.group({
-            date: null,
+            datetime: null,
             time: null
         });
 
         this.dateTimeRangeForm = this.fb.group({
-            start: null,
-            end: null,
+            startDateTime: null,
+            endDateTime: null,
             startTime: null,
             endTime: null
         });
-    }
 
-    /**
-     * Triggered when changing date in calendar popup
-     */
-    public onDateTimeInput(event: MatDatepickerInputEvent<unknown>): void {
-        const time = this.dateTimeForm.get('time').value as Date;
-        this.dateTimeForm.get('date').setValue(set(event.value as Date, { hours: time.getHours(), minutes: time.getMinutes() }));
-    }
+        this.dateTimeForm.valueChanges.pipe(
+            takeUntil(this.destroyed$)
+        ).subscribe((values: { time: Date; datetime: Date }) => {
+            let datetime = values.datetime;
+            if (!datetime) {
+                datetime = new Date();
+            }
+            datetime.setHours(values.time?.getHours() || 0);
+            datetime.setMinutes(values.time?.getMinutes() || 0);
+            datetime.setSeconds(values.time?.getSeconds() || 0);
+            this.dateTimeForm.controls.datetime.setValue(datetime, { emitEvent: false, emitModelToViewChange: true });
+            console.log(datetime);
+        });
 
-    /**
-     * Triggered when changing date in input field or calendar popup
-     */
-    public onDateTimeChange(event: MatDatepickerInputEvent<unknown>): void {
-        const inputValue = (event.targetElement as HTMLInputElement).value;
-        if (isMatch(inputValue, dateTimeFormat)) {
-            const newDate = parse(inputValue, dateTimeFormat, startOfToday());
-            this.dateTimeForm.get('date').setValue(newDate);
-            this.dateTimeForm.get('time').setValue(newDate);
-        }
-    }
+        const startDateTime$ = combineLatest([of(this.dateTimeRangeForm.controls.startDateTime), this.dateTimeRangeForm.controls.startDateTime.valueChanges, this.dateTimeRangeForm.controls.startTime.valueChanges]);
+        const endDateTime$ = combineLatest([of(this.dateTimeRangeForm.controls.endDateTime), this.dateTimeRangeForm.controls.endDateTime.valueChanges, this.dateTimeRangeForm.controls.endTime.valueChanges]);
+        merge(startDateTime$, endDateTime$).pipe(
+            takeUntil(this.destroyed$)
+        ).subscribe(([control, dateTime, time]: [AbstractControl, Date, Date]) => {
+            if (!dateTime) {
+                dateTime = new Date();
+            }
 
-    /**
-     * Triggered when closing the calendar popup
-     */
-    public onDateTimeClosed(dateTimePicker: MatDatepicker<unknown>): void {
-        // copy the date field value to the time field (useful if the timepicker was updated but the datepicker popup was closed without applying the change)
-        this.dateTimeForm.get('time').setValue((dateTimePicker.datepickerInput as MatDatepickerInput<Date>).value);
-    }
-
-    /**
-     * Triggered when changing start or end date in calendar popup
-     */
-    public onDateTimeRangeInput(event: MatDatepickerInputEvent<unknown>, dateFieldName: string): void {
-        const time = this.dateTimeRangeForm.get(`${dateFieldName}Time`).value as Date;
-        this.dateTimeRangeForm.get(dateFieldName).setValue(set(event.value as Date, { hours: time.getHours(), minutes: time.getMinutes() }));
-    }
-
-    /**
-     * Triggered when changing start or end date in input field or calendar popup
-     */
-    public onDateTimeRangeChange(event: MatDatepickerInputEvent<unknown>, dateFieldName: string): void {
-        const inputValue = (event.targetElement as HTMLInputElement).value;
-        if (isMatch(inputValue, dateTimeFormat)) {
-            const newDate = parse(inputValue, dateTimeFormat, startOfToday());
-            this.dateTimeRangeForm.get(dateFieldName).setValue(newDate);
-            this.dateTimeRangeForm.get(`${dateFieldName}Time`).setValue(newDate);
-        }
-    }
-
-    /**
-     * Triggered when closing the calendar popup
-     */
-    public onDateTimeRangeClosed(dateTimeRangePicker: MatDateRangePicker<unknown>): void {
-        const dateRange = (dateTimeRangePicker.datepickerInput as MatDateRangeInput<Date>).value;
-        if (this.dateTimeRangeApplied) {
-            // workaround to apply time to date since the apply event is not fired when the date range is not changed
-            const startTime = this.dateTimeRangeForm.get('startTime').value as Date;
-            this.dateTimeRangeForm.get('start').setValue(set(dateRange.start, { hours: startTime.getHours(), minutes: startTime.getMinutes() }));
-            const endTime = this.dateTimeRangeForm.get('endTime').value as Date;
-            this.dateTimeRangeForm.get('end').setValue(set(dateRange.end, { hours: endTime.getHours(), minutes: endTime.getMinutes() }));
-        } else {
-            // copy the date field values to each time fields (useful if the timepicker was updated but the datepicker popup was closed without applying the change)
-            this.dateTimeRangeForm.get('startTime').setValue(dateRange.start);
-            this.dateTimeRangeForm.get('endTime').setValue(dateRange.end);
-        }
-        this.dateTimeRangeApplied = false;
+            dateTime.setHours(time?.getHours() || 0);
+            dateTime.setMinutes(time?.getMinutes() || 0);
+            dateTime.setSeconds(time?.getSeconds() || 0);
+            control.setValue(dateTime, { emitEvent: false, emitModelToViewChange: true });
+            console.log(dateTime);
+        });
     }
 }
