3aca0f4600c3d8c0c2c994f789418f02bc835c98
sdil <sdil@hcuge.ch>
2021-11-19 11:14:09 +0100

    fix(): revert try fix importcost plugin failure with date-fns-utils.ts/locale import, not working

--- projects/deja-js/component/core/date-fns/date-fns-utils.ts
+++ projects/deja-js/component/core/date-fns/date-fns-utils.ts
@@ -6,7 +6,7 @@
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
 import { format, Locale } from 'date-fns';
-import enUS from 'date-fns/locale/en-US/index';
+import { enUS } from 'date-fns/locale';
 
 // default Locale in date-fns is en-US: https://github.com/date-fns/date-fns/blob/master/src/format/index.js
 let globalLocale: Locale = enUS;
