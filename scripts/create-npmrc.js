72823d64a5781f9ee16a270f4e71a89ca8309d5e
Serge <sdil@hcuge.ch>
2022-01-19 21:05:25 +0100

    ci(): try using github actions

--- scripts/create-npmrc.js
+++ scripts/create-npmrc.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.writeFileSync('.npmrc', `@deja-js:registry=//registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}`);
+fs.writeFileSync('.npmrc', `@deja-js:registry=https://registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}`);
