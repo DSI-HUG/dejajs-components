9043a7f0fd6c0347ccfb2f0e3faedb0bdf3b9f1b
lday <laurentbertrand.dulary@hcuge.ch>
2022-04-07 11:22:29 +0200

    fix(material-colors): correct license

--- projects/deja-js/component/core/graphics/material-colors.service.ts
+++ projects/deja-js/component/core/graphics/material-colors.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 /*
  *  @license
  *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
@@ -6,6 +5,7 @@
  *  Use of this source code is governed by an Apache-2.0 license that can be
  *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
  */
+/* eslint-disable @typescript-eslint/naming-convention */
 
 import { Injectable } from '@angular/core';
 
